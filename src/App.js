e9afdc50878761d85085cba5f00fadc8944088be
INVIT <invit@MacBook-Pro-de-Marion.local>
2020-12-03 15:08:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Row from './Row'
+import requests from './request'
+import Banner from './Banner'
+import NavBar from './NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar/>
+      <Banner/>
+      <Row 
+        title='NETFLIX ORIGINALS' 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+        />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
+      <Row title='Top rated' fetchUrl={requests.fetchTopRated}/>
+      <Row title='Action' fetchUrl={requests.fetchActionMovies}/>
+      <Row title='Comedy' fetchUrl={requests.fetchComedyMovies}/>
+      <Row title='Horror' fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title='Romance' fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
